1a0529aa6ef00e72e681ce6566fa7628efba09e4
Shreya <shreya11081999@gmail.com>
2023-12-01 14:51:55 +0530

    Initial commit

---
+++ src/main/java/com/demo/wallet/AdvanceWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceWalletApiApplication.class, args);
+	}
+
+}
